c151ce908f41a6d6eb31233c7810cafe5ebcbf08
Nikita Osadchyi <nikitaosadchyi7@gmail.com>
2022-02-19 15:47:25 +0200

    markup and lightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,4 +5,20 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+
+const galleryEl = document.querySelector('.gallery');
+
+function renderList(galleryItems) {
+    const markup = galleryItems.map(({ preview, original, description }) => `
+    <a class="gallery__item"href="${original}"> 
+    <img class="gallery__image" src ="${preview}" alt="${description}"/>
+    </a>
+    `).join('');
+    galleryEl.insertAdjacentHTML('afterbegin', markup);
+}
+
+renderList(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionsDelay: 250 });
+
 console.log(galleryItems);
